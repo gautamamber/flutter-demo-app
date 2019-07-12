c9d25f5bdd859f187dceb488d70e0a7eae860883
gautamamber <ambergautam1@gmail.com>
2019-07-13 00:59:34 +0530

    hello world

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  // for run a app, pass app widget (class), pass class object
+  // instance of materialApp
+  // provide home and new text in home
+  runApp(new MaterialApp(home: new Text("Hello world"),));
+}
